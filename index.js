5e3e2d1bcb61c6ac6407cb009615c6acb976d51d
David MacPhail <david.macphail@icloud.com>
2020-05-19 21:13:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ function happyHolidays (string) {
 function happyHolidaysTo (name) {
   return (`Happy holidays, ${name}!`)
 }
+
+function happyCustomHolidayTo (string) {
+  return (`Happy ${holiday}, ${name}!`)
+}
